33bccaafd2cb7a5a738c82916c1689696a0c88a9
Tomi Setiawan <90341045+XtomiSN@users.noreply.github.com>
2021-11-03 13:26:09 +0700

    Add files via upload

---
+++ MusicAndVideo/helpers/decorators.py
@@ -0,0 +1,21 @@
+from typing import Callable
+
+from MusicAndVideo.helpers.admins import get_administrators
+from pyrogram import Client
+from pyrogram.types import Message
+
+from config import SUDO_USERS
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
+    return decorator
