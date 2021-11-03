68dd821534a38524222ff0153aebef2055c4d308
Tomi Setiawan <90341045+XtomiSN@users.noreply.github.com>
2021-11-03 13:59:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ if os.path.exists(".env"):
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", "$")
 DOWN_PATH = os.getenv("./downloads")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 
