2745dd7279a741089befd906a6dde07ae0edf5fd
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-11-05 12:28:42 +0700

    Update play.py

--- MusicAndVideo/play.py
+++ MusicAndVideo/play.py
@@ -41,7 +41,7 @@ IMAGE = random.choice(AMBILFOTO)
 THUMBNAIL = Image.open(io.BytesIO(requests.get(IMAGE).content))
 APP = ImageDraw.Draw(THUMBNAIL)
 IMAGE_THUMBNAIL = "final.jpg"
-THUMBNAIL.save(APP, IMAGE_THUMBNAIL, "jpg")
+APP.save(IMAGE_THUMBNAIL, "jpg")
 
 # music player
 def ytsearch(query):
