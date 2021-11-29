83e3be9cd5553b736f6ad11f04803773179266b9
TOMI SETIAWAN <87267285+Good-Boys-Exe@users.noreply.github.com>
2021-11-29 13:01:33 +0700

    Update handlers.py

--- MusicAndVideo/helpers/handlers.py
+++ MusicAndVideo/helpers/handlers.py
@@ -68,7 +68,7 @@ async def skip_item(chat_id, h):
 @call_py.on_stream_end()
 async def stream_end_handler(_, u: Update):
     if isinstance(u, StreamAudioEnded):
-        chat_id = update.chat_id
+        chat_id = u.chat_id
         print(chat_id)
         await skip_current_song(chat_id)
 
