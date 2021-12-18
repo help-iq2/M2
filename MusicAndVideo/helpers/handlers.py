66f003f7eb1e8f0ae855fbb227e9bd4dabc32b8b
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 16:22:53 +0300

    Update handlers.py

--- MusicAndVideo/helpers/handlers.py
+++ MusicAndVideo/helpers/handlers.py
@@ -2,17 +2,10 @@ from pyrogram.raw.base import Update
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import (
-    HighQualityAudio,
-    HighQualityVideo,
-    LowQualityVideo,
-    MediumQualityVideo,
-)
+from pytgcalls.types.input_stream.quality import HighQualityAudio,    HighQualityVideo,    LowQualityVideo,    MediumQualityVideo
 from pytgcalls.types.stream import StreamAudioEnded, StreamVideoEnded
-
 from config import call_py
 from MusicAndVideo.helpers.queues import QUEUE, clear_queue, get_queue, pop_an_item
-
 
 async def skip_current_song(chat_id):
     if chat_id in QUEUE:
@@ -28,12 +21,7 @@ async def skip_current_song(chat_id):
             type = chat_queue[1][3]
             Q = chat_queue[1][4]
             if type == "Audio":
-                await call_py.change_stream(
-                    chat_id,
-                    AudioPiped(
-                        url,
-                    ),
-                )
+                await call_py.change_stream(                    chat_id,                    AudioPiped(                        url,                    ),                )
             elif type == "Video":
                 if Q == 720:
                     hm = HighQualityVideo()
@@ -41,15 +29,11 @@ async def skip_current_song(chat_id):
                     hm = MediumQualityVideo()
                 elif Q == 360:
                     hm = LowQualityVideo()
-                await call_py.change_stream(
-                    chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
-                )
+                await call_py.change_stream(                    chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)                )
             pop_an_item(chat_id)
             return [songname, link, type]
     else:
         return 0
-
-
 async def skip_item(chat_id, h):
     if chat_id in QUEUE:
         chat_queue = get_queue(chat_id)
@@ -63,27 +47,18 @@ async def skip_item(chat_id, h):
             return 0
     else:
         return 0
-
-
 @call_py.on_stream_end()
 async def on_end_handler(_, update: Update):
     if isinstance(update, StreamAudioEnded):
         chat_id = update.chat_id
         print(chat_id)
         await skip_current_song(chat_id)
-
-
 @call_py.on_stream_end()
 async def on_end_handler(_, update: Update):
     if isinstance(update, StreamVideoEnded):
         chat_id = update.chat_id
         print(chat_id)
         await skip_current_song(chat_id)
-
-
-# Ketika seseorang mengakhiri Obrolan Suara tanpa menghentikan Pemutaran
-
-
 @call_py.on_closed_voice_chat()
 async def close_handler(client: PyTgCalls, chat_id: int):
     if chat_id in QUEUE:
