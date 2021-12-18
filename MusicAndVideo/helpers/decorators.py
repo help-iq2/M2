f9c1f71957ad9fad60d1d6107846408196229d90
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 16:20:44 +0300

    Update decorators.py

--- MusicAndVideo/helpers/decorators.py
+++ MusicAndVideo/helpers/decorators.py
@@ -1,12 +1,8 @@
 from typing import Callable
-
 from pyrogram import Client
 from pyrogram.types import Message
-
 from config import SUDO_USERS
 from MusicAndVideo.helpers.admins import get_administrators
-
-
 def authorized_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         if message.from_user.id in SUDO_USERS:
