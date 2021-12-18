f695253cd59306d3239fc17918d23ebbc72e3b31
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 03:54:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 import asyncio
-
 from pytgcalls import idle
-
 from config import call_py
 from MusicAndVideo.quote import arq
-
-
 async def main():
     await call_py.start()
     print(
         """
     ------------------
-   | Userbot Started! |
+   | ميوزك تليثون الان شغال ! |
     ------------------
 """
     )
     await idle()
     await arq.close()
-
-
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
