69c5dd438ef221a9fac2fb26ecc9f760d67dba8c
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 14:38:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 from pytgcalls import idle
 from config import call_py
-from MusicAndVideo.quote import arq
+from MusicAndVideo.play import arq
 async def main():
     await call_py.start()
     print(
