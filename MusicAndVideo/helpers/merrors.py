0f93544c0e11d5c079b404e43567d9c158d80c6c
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 16:24:40 +0300

    Update merrors.py

--- MusicAndVideo/helpers/merrors.py
+++ MusicAndVideo/helpers/merrors.py
@@ -1,11 +1,8 @@
 import sys
 import traceback
 from functools import wraps
-
 from pyrogram import Client
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-
-
 def split_limits(text):
     if len(text) < 2048:
         return [text]
@@ -24,7 +21,6 @@ def split_limits(text):
 
     return result
 
-
 def capture_err(func):
     @wraps(func)
     async def capture(client, message, *args, **kwargs):
@@ -35,21 +31,11 @@ def capture_err(func):
             return
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
-            )
+            errors = traceback.format_exception(                etype=exc_type,                value=exc_obj,                tb=exc_tb,            )
             error_feedback = split_limits(
-                "**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(
-                    0 if not message.from_user else message.from_user.id,
-                    0 if not message.chat else message.chat.id,
-                    message.text or message.caption,
-                    "".join(errors),
-                ),
-            )
+                "**هناك خطأ ** | `{}` | `{}`\n\n```{}```\n\n```{}```\n".format(                    0 if not message.from_user else message.from_user.id,                    0 if not message.chat else message.chat.id,                    message.text or message.caption,                    "".join(errors),                ),            )
             for x in error_feedback:
-                await Client.send_message(-1001568994954, x)
+                await Client.send_message(-1001622191409, x)
             raise err
 
     return capture
