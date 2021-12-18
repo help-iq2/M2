ac5e5204b3c487edaad466d2cb1cd4dd7af218ed
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 17:10:11 +0300

    Update تحميل.py

--- MusicTelethon/تحميل.py
+++ MusicTelethon/تحميل.py
@@ -18,7 +18,7 @@ async def song(client, message: Message):
     if not urlissed:
         await client.send_message(            message.chat.id,            "صيغة الأمر غير صالحة!",        )
         return
-    pablo = await client.send_message(message.chat.id, f"**🔎 Mencari** `{urlissed}`")
+    pablo = await client.send_message(message.chat.id, f"**🔎 جاري تحميل ** `{urlissed}`")
     search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
     mi = search.result()
     mio = mi["search_result"]
