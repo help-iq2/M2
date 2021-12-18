bf973b8ca8d414aa3585404be08fb551b1881dbf
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 16:39:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ if os.path.exists(".env"):
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", "/")
+HNDLR = os.getenv("HNDLR", "$")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 contact_filter = filters.create(    lambda _, __, message: (message.from_user and message.from_user.is_contact)    or message.outgoing)
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicTelethon"))
