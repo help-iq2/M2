1809bc5e1360e2852241888669d5332cad0eaae4
telenothelp <87152880+telenothelp@users.noreply.github.com>
2021-12-18 03:56:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,16 @@
 import os
-
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
-
-# For Local Deploy
+# للنشر المحلي
 if os.path.exists(".env"):
     load_dotenv(".env")
-
-# Necessary Vars
+# الفارات
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "/")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
-
-
-contact_filter = filters.create(
-    lambda _, __, message: (message.from_user and message.from_user.is_contact)
-    or message.outgoing
-)
-
+contact_filter = filters.create(    lambda _, __, message: (message.from_user and message.from_user.is_contact)    or message.outgoing)
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicAndVideo"))
 call_py = PyTgCalls(bot)
